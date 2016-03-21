8926b0f3b7f53eea51b61cb463dea298574a41e6
GavrikGal <gavrik.gal@gmail.com>
2016-03-22 02:50:39 +0300

    Объект пока не получается((

--- html/js/smartObject.js
+++ html/js/smartObject.js
@@ -18,33 +18,88 @@ var SmartObject = function (id, content) {
 
   if (content !== undefined) {
     this.content.remove();
-    this.content = content.clone();
+    this.content = $(content).clone();
   }
 
-  this.content.appendTo(this.wrapper);
+	this.content.appendTo(this.wrapper);
 
-  initSmartObject(this.wrapper, this.draggableWrapper);
+	if (content !== undefined) {
+		hideSmartObject(this);
 
-  this.objectWidth = this.content.width();
-  this.objectHeight = this.content.height();
-  this.wrapper.width(this.objectWidth);
-  this.wrapper.height(this.objectHeight);
-  this.content.css('width', '100%');
-  this.content.css('height', '100%');
-  this.objectCurrentRotation = $(this.wrapper).data('currentRotation');
+    content.find("img").load(initSmartObject(this));
 
-  this.width = function () {
-    return this.objectWidth;
-  };
-  this.height = function () {
-    return this.objectHeight;
-  };
-  this.currentRotation = function () {
-    return this.objectCurrentRotation;
-  };
+  } else {
+		initSmartObject(this);
+	}
+	this.objectCurrentRotation = $(this.wrapper).data('currentRotation');
+
+	this.width = function () {
+		return this.objectWidth;
+	};
+	this.height = function () {
+		return this.objectHeight;
+	};
+	this.currentRotation = function () {
+		return this.objectCurrentRotation;
+	};
+
+};
+
+function hideSmartObject (smartObject) {
+	smartObject.draggableWrapper.hide();
+	smartObject.wrapper.hide();
+	smartObject.content.hide();
 }
 
-function initSmartObject(wrapper, draggableWrapper) {
+function showSmartObject (smartObject) {
+	smartObject.draggableWrapper.show();
+	smartObject.wrapper.show();
+	smartObject.content.show();
+}
+
+function initSmartObject(smartObject) {
+	"use strict";
+	console.log(smartObject.content.prop("tagName") + " width: " + smartObject.content.width());
+	// Устанавливаем случайное положение и угол поворота для объекта
+	// TODO заменить на загрузку начального положения
+	var left = Math.floor(Math.random() * 450 + 100),
+		top = Math.floor(Math.random() * 100 + 100),
+		angle = Math.floor(Math.random() * 60 - 30);
+	$(smartObject.draggableWrapper).css('left', left + 'px');
+	$(smartObject.draggableWrapper).css('top', top + 'px');
+	$(smartObject.wrapper).css('transform', 'rotate(' + angle + 'deg)');
+	$(smartObject.wrapper).css('-moz-transform', 'rotate(' + angle + 'deg)');
+	$(smartObject.wrapper).css('-webkit-transform', 'rotate(' + angle + 'deg)');
+	$(smartObject.wrapper).css('-o-transform', 'rotate(' + angle + 'deg)');
+	$(smartObject.wrapper).data('currentRotation', angle * Math.PI / 180);
+
+	// Делаем объект перемещаемым
+	// TODO перенсти на кнопку
+	$(smartObject.draggableWrapper).draggable({ containment: 'parent', stack: '.smatrObject', cursor: 'pointer', start: dragStart() });
+
+	// Делаем объект способным вращаться
+	$(smartObject.wrapper).mousedown(startRotate);
+
+	// Устанавливаем z-index больше, чем у объектов, уже размещенных на столе
+	$(smartObject.wrapper).css('z-index', newImageZIndex++);
+
+
+	showSmartObject(smartObject);
+
+	smartObject.objectWidth = smartObject.content.width();
+	smartObject.objectHeight = smartObject.content.height();
+	smartObject.wrapper.width(smartObject.objectWidth);
+	smartObject.wrapper.height(smartObject.objectHeight);
+	smartObject.content.css('width', '100%');
+	smartObject.content.css('height', '100%');
+	if (smartObject.content.get(0).complete) {
+		$(smartObject.content).trigger("load");
+	}
+
+
+}
+
+/*function initSmartObject(wrapper, draggableWrapper) {
 	"use strict";
 	// Устанавливаем случайное положение и угол поворота для объекта
 	// TODO заменить на загрузку начального положения
@@ -68,4 +123,4 @@ function initSmartObject(wrapper, draggableWrapper) {
 
 	// Устанавливаем z-index больше, чем у объектов, уже размещенных на столе
 	$(wrapper).css('z-index', newImageZIndex++);
-}
+}*/
